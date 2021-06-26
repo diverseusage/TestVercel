6eafc72736e3a7c99cc50187c8b652dd77087673
DiverseUsage <diverseusage@gmail.com>
2021-06-26 19:10:27 +0800

    remove rule

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,42 +1,58 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { storage } from '../../firebase'
-import { v4 as generateUuid } from 'uuid';
-import mimeLookup from 'mime-types';
-import { base } from '../../server/image'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { storage } from "../../firebase";
+import { v4 as generateUuid } from "uuid";
+import mimeLookup from "mime-types";
+import { base } from "../../server/image";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-const FIREBASE_STORAGE_UPLOAD_DIRECTORY= "uploads";
+const FIREBASE_STORAGE_UPLOAD_DIRECTORY = "uploads";
 const base64String = base === null ? "" : base.toString();
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const mimeType = base64String?.match(/data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/)[1];
-  const fileExtension = mimeLookup.extension(mimeType)
+  const mimeType = base64String?.match(
+    /data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/
+  )[1];
+  const fileExtension = mimeLookup.extension(mimeType);
   const fileName = `${generateUuid()}.${fileExtension}`;
-  const base64EncodedImageString = base64String.split(';base64,').pop();
-  const imageBuffer = Buffer.from(base64EncodedImageString, 'base64');
+  const base64EncodedImageString = base64String.split(";base64,").pop();
+  const imageBuffer = Buffer.from(base64EncodedImageString, "base64");
 
+  let result = {};
+
+  console.log("Before getting bucket");
   let bucket = storage.bucket();
+  console.log(
+    "Bucket path: ",
+    `${FIREBASE_STORAGE_UPLOAD_DIRECTORY}/${fileName}`
+  );
   const file = bucket.file(`${FIREBASE_STORAGE_UPLOAD_DIRECTORY}/${fileName}`);
-  file.save(imageBuffer, {
-    metadata: { 
+
+  console.log("Before save file");
+  file.save(
+    imageBuffer,
+    {
       metadata: {
-        contentType: mimeType,
-        firebaseStorageDownloadTokens: generateUuid()
-      }
+        metadata: {
+          contentType: mimeType,
+          firebaseStorageDownloadTokens: generateUuid(),
+        },
+      },
+      public: true,
+      validation: "md5",
     },
-    public: true,
-    validation: 'md5'
-  }, (error) => {
+    (error) => {
       if (error) {
-          console.log(error)
+        console.log(error);
       }
-  });
-  res.status(200).json({ name: 'John Doe' })
+    }
+  );
+
+  res.status(200).json({ name: "John Doe" });
 }
